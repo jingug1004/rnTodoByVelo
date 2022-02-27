5eba404b602d529ef60a9f0e586b4bc99bd932b3
jingug1004 <jingug1004@gmail.com>
2022-02-27 15:00:11 +0900

    page.160 할일 목록 만들기 1 끝냈음. 프로젝트 UI 구성에 필요한 준비 작업을 대부분 마쳤다고 하네..

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import DateHead from './components/DateHead';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
@@ -12,9 +12,13 @@ function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
-        <DateHead date={today} />
-        <Empty />
-        <AddTodo />
+        <KeyboardAvoidingView
+          behavior={Platform.select({ ios: 'padding', android: undefined })}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -24,6 +28,9 @@ const styles = StyleSheet.create({
   block: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
   },
 });
 
